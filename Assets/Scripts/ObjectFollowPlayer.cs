e66c16f894666fef6af9ae5c92e297db1bbe88ea
Letail <francisco.tadeu.torres@gmail.com>
2020-09-07 20:39:22 +0100

    Some Testing

---
+++ Assets/Scripts/ObjectFollowPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectFollowPlayer : MonoBehaviour
+{
+    public Transform playerSphere;
+    private Vector3 offset;
+
+    void Start()
+    {
+        offset = playerSphere.transform.position - transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = playerSphere.transform.position + offset;
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,80 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private PlayerControlsAsset playerControls;
+    private Rigidbody rb;
+
+    public float maxSpeed;
+
+    [Header("Move with Force")]
+    public bool useForce;
+    public float speed;
+
+    [Header("Move with Acceleration")]
+    public bool useAcceleration;
+    public float speedAcc;
+
+    private void Awake()
+    {
+        playerControls = new PlayerControlsAsset();
+        rb = GetComponent<Rigidbody>();
+    }
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        //Read Value
+        Vector2 movementInput = playerControls.Player.Move.ReadValue<Vector2>();
+        Vector3 movementVector = new Vector3(movementInput.x, 0, movementInput.y);
+        //Move Player
+
+        if(useForce) MoveWithForce(movementVector);
+        if(useAcceleration) MoveWithAcceleration(movementVector);
+    }
+
+    private void MoveWithAcceleration(Vector3 movementVector)
+    {
+        float speedDifference = rb.velocity.magnitude - maxSpeed;
+        if (speedDifference > 0)
+        {
+            rb.AddForce(movementVector * -speedDifference, ForceMode.Acceleration);
+        }
+        else
+        {
+            rb.AddForce(movementVector * speedAcc, ForceMode.Acceleration);
+        }
+    }
+
+    void MoveWithForce(Vector3 movementVector)
+    {
+        float speedDifference = rb.velocity.magnitude - maxSpeed;
+        if (speedDifference > 0)
+        {
+            rb.AddForce(movementVector * -speedDifference, ForceMode.Force);
+        }
+        else
+        {
+            rb.AddForce(movementVector * speed, ForceMode.Force);
+        }
+    }
+
+    private void OnEnable()
+    {
+        playerControls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerControls.Disable();
+
     }
 }
