2c156ad2edcf1b14e84ef148c2f81f372de0510e
Letail <francisco.tadeu.torres@gmail.com>
2020-09-08 15:49:28 +0100

    Bumper ligths up when touched

---
+++ Assets/Scripts/BumperEffects.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BumperEffects : MonoBehaviour
+{
+    public GameObject bumperRing;
+    public float colourChangeTime;
+    public Color colorToChangeTo;
+
+    private Material bumperRingMaterial;
+    private Color initialBumperRingColor;
+
+    private void Awake()
+    {
+        bumperRingMaterial = bumperRing.GetComponent<Renderer>().material;
+        initialBumperRingColor = bumperRingMaterial.color;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        AnimateRing();
+    }
+
+    private void AnimateRing()
+    {
+        StartCoroutine(ColourChange());
+
+    }
+
+    IEnumerator ColourChange()
+    {
+        bumperRingMaterial.SetColor("_BaseColor", colorToChangeTo);
+
+        yield return new WaitForSeconds(colourChangeTime);
+
+        bumperRingMaterial.SetColor("_BaseColor", initialBumperRingColor);
+
+    }
+}
