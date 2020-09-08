e870c83a9bb86b46b7cca22c1dc9fd4520f9328a
Letail <francisco.tadeu.torres@gmail.com>
2020-09-08 13:19:25 +0100

    Changed scene and some layers
    Added ground layer to grounds

---
+++ Assets/Scripts/Bounce.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Bounce : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float bounceForce;
+    public int groundLayer;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();    
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.layer != groundLayer)
+        {
+            Vector3 bounceBack = transform.position - collision.transform.position;
+
+            foreach (var point in collision.contacts)
+            {
+                rb.AddForceAtPosition(point.normal * bounceForce, point.point, ForceMode.Impulse);
+            }
+        }
+        
+    }
+}
