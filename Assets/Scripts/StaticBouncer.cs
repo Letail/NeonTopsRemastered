c8b09b629aa19c18137ddab5858901000bd7a1ce
Letail <francisco.tadeu.torres@gmail.com>
2020-09-08 13:42:27 +0100

    Changed Bumper prefab

---
+++ Assets/Scripts/StaticBouncer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaticBouncer : MonoBehaviour
+{
+    public float bounceForce;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Rigidbody rb;
+
+        if (collision.gameObject.GetComponent<Rigidbody>() != null)
+        {
+            rb = collision.gameObject.GetComponent<Rigidbody>();
+
+            foreach (var point in collision.contacts)
+            {
+                rb.AddForceAtPosition(point.normal * bounceForce, point.point, ForceMode.Impulse);
+            }
+        }
+
+        
+        
+
+    }
+}
