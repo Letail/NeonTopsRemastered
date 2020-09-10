4756bf327ca94179e0d1714738f45858dd922178
Letail <francisco.tadeu.torres@gmail.com>
2020-09-10 15:19:32 +0100

    Imported some simple audio
    Also gave bumpers a sound effect

---
+++ Assets/Scripts/PlayAudioOnCollisionEnter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+[RequireComponent(typeof(Collider))]
+public class PlayAudioOnCollisionEnter : MonoBehaviour
+{
+    AudioSource audioSource;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        audioSource.PlayOneShot(audioSource.clip);
+    }
+
+}
