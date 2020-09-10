21e5f6c44c8d20e1e5d528fc924e7db085a44b70
Letail <francisco.tadeu.torres@gmail.com>
2020-09-10 13:42:32 +0100

    Implemented events with delegates on the OutOfArenaTrigger.
    I also moved the arena to the center of the scene.

---
+++ Assets/Scripts/OutOfArenaTrigger.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider))]
+public class OutOfArenaTrigger : MonoBehaviour
+{
+    public delegate void OnPlayerOutOfArena(Transform trans);
+    public event OnPlayerOutOfArena OnPlayerOutOfArenaEvent;
+
+    private void Awake()
+    {
+        OnPlayerOutOfArenaEvent += PlayerLeftArena;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        OnPlayerOutOfArenaEvent?.Invoke(other.transform);
+    }
+
+    private void PlayerLeftArena(Transform playerTrans)
+    {
+        Debug.Log("Player " + playerTrans.name + " left arena");
+    }
+
+}
