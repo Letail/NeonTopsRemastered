9a0f005872ecaee68beee203036d6d5fd4c44dd9
Letail <francisco.tadeu.torres@gmail.com>
2020-09-12 16:03:19 +0100

    commented this script

--- Assets/Scripts/PlayerPropertiesHolder.cs
+++ Assets/Scripts/PlayerPropertiesHolder.cs
@@ -2,14 +2,14 @@
 
 public class PlayerPropertiesHolder : MonoBehaviour
 {
+    //These properties are to be set by SetPlayersPropertiesOnSpawn.cs
+    //They're supposed to be left unassigned, since they will be different
+    //depending on which person is controlling this player character
+    [HideInInspector]
     public PlayerProperties playerProperties;
 
     private void Start()
     {
-        if(playerProperties == null)
-        {
-            Debug.LogError("Player doesn't have it's properties set!");
-            //throw new System.Exception("Player doesn't have it's properties set!");
-        }
+        if(playerProperties == null) Debug.LogError("Player doesn't have it's properties set!");
     }
 }
