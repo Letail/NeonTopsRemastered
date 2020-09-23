4588d2e6ed5af8c9847a7790a3a9fdf6e8a0b6dc
Letail <francisco.tadeu.torres@gmail.com>
2020-09-23 16:09:27 +0100

    Changed // to ///

--- Assets/Scripts/Player Related/PlayerPropertiesHolder.cs
+++ Assets/Scripts/Player Related/PlayerPropertiesHolder.cs
@@ -2,9 +2,9 @@
 
 public class PlayerPropertiesHolder : MonoBehaviour
 {
-    //These properties are to be set by SetPlayersPropertiesOnSpawn.cs
-    //They're supposed to be left unassigned, since they will be different
-    //depending on which person is controlling this player character
+    ///These properties are to be set by SetPlayersPropertiesOnSpawn.cs
+    ///They're supposed to be left unassigned, since they will be different
+    ///depending on which person is controlling this player character
     [HideInInspector]
     public PlayerProperties playerProperties;
 
