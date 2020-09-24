3d78e65f6fcddd733426faf07572f0aa472ec941
Letail <francisco.tadeu.torres@gmail.com>
2020-09-24 17:39:08 +0100

    Initial particle test

---
+++ Assets/Scripts/ParticleTest.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleTest : MonoBehaviour
+{
+    public GameObject particlePrefab;
+
+    private void Start()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Instantiate(particlePrefab, collision.contacts[0].point, Quaternion.identity);
+    }
+}
