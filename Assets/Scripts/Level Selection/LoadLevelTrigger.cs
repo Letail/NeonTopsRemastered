dbec29cf6f553d0f56cc948fd5f5d36e6cba9269
Letail <francisco.tadeu.torres@gmail.com>
2020-09-26 22:38:05 +0100

    Created a simple Scene Selection scene

---
+++ Assets/Scripts/Level Selection/LoadLevelTrigger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(Collider))]
+public class LoadLevelTrigger : MonoBehaviour
+{
+    [SerializeField]
+    private Collider collider;
+    public string sceneToLoad;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        SceneManager.LoadScene(sceneToLoad);
+    }
+}
