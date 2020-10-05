4b63364333d6a21d5647d14f7ac76f49ebc41e32
Letail <francisco.tadeu.torres@gmail.com>
2020-10-05 22:15:01 +0100

    Tuned RB settings and added Force Towards Other Players
    Controls are now tighter and the spinning tops now have force applied to them if they are close to each other, to facilitate hitting other players

---
+++ Assets/Scripts/Player Related/ForceTowardsOtherPlayers.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ForceTowardsOtherPlayers : MonoBehaviour
+{
+    [SerializeField]
+    private float forceAmount;
+    [SerializeField]
+    private float radiusOfInfluence;
+
+    private List<Transform> otherPlayers;
+    private Rigidbody rb;
+    private Vector3 dirToOtherPlayer;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        otherPlayers = new List<Transform>();
+        PlayerInputManager.instance.onPlayerJoined += OnPlayerJoined;
+        PlayerInputManager.instance.onPlayerLeft += OnPlayerLeft;
+    }
+    void Update()
+    {
+        foreach (Transform player in otherPlayers)
+        {
+            if (Vector3.Distance(player.position, transform.position ) < radiusOfInfluence)
+            {
+                dirToOtherPlayer = player.position - transform.position;
+                rb.AddForce(dirToOtherPlayer * forceAmount, ForceMode.Force);
+            }
+        }
+    }
+
+    public void OnPlayerJoined(PlayerInput playerInput)
+    {
+        otherPlayers.Add(playerInput.transform);
+    }
+
+    public void OnPlayerLeft(PlayerInput playerInput)
+    {
+        otherPlayers.Remove(playerInput.transform);
+    }
+
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, radiusOfInfluence);
+    }
+
+    private void OnDisable()
+    {
+        PlayerInputManager.instance.onPlayerJoined -= OnPlayerJoined;
+        PlayerInputManager.instance.onPlayerLeft -= OnPlayerLeft;
+
+    }
+}
