e090ae043045f02dd2a1c80f7426a577a87b139e
Letail <francisco.tadeu.torres@gmail.com>
2020-10-12 17:11:37 +0100

    New stands for player skin selection

---
+++ Assets/Scripts/UI/PlayerSkinSelectionStands.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerSkinSelectionStands : MonoBehaviour
+{
+    [SerializeField]
+    private List<GameObject> stands;
+
+    private void Start()
+    {
+        foreach (var item in stands)
+        {
+            item.SetActive(false);
+        }
+        PlayerInputManager.instance.onPlayerJoined += ActivateStand;    
+    }
+
+
+
+    private void ActivateStand(PlayerInput playerInput)
+    {
+        foreach (var item in stands)
+        {
+            if(item.activeSelf == false)
+            {
+                item.SetActive(true);
+                return;
+            }
+        }
+    }
+
+
+    private void OnDisable()
+    {
+        PlayerInputManager.instance.onPlayerJoined -= ActivateStand;
+
+    }
+
+
+}
