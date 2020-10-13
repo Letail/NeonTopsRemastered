742c6697f36585c287671db066ee398fcd800ee7
Letail <francisco.tadeu.torres@gmail.com>
2020-10-13 01:22:34 +0100

    Started Implementing showcase mode for player prefab

---
+++ Assets/Scripts/Player Related/PlayerShowcaseMode.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShowcaseMode : MonoBehaviour
+{
+    private bool isInShowcaseMode;
+
+    [SerializeField]
+    private Rigidbody rb;
+    [SerializeField]
+    private PlayerMovement playerMovement;
+    [SerializeField]
+    private SpawnCharacterVisualsPrefab visualsPrefab;
+    private GameObject prefabGameObject;
+
+    public void EnableShowcase()
+    {
+        if(!isInShowcaseMode)
+        {
+            isInShowcaseMode = true;
+            rb.isKinematic = true;
+            playerMovement.enabled = false;
+            prefabGameObject = visualsPrefab.GetPrefab();
+            prefabGameObject.GetComponent<TrailRenderer>().enabled = false;
+            prefabGameObject.GetComponent<Light>().enabled = false;
+
+        }
+    }
+
+}
--- Assets/Scripts/UI/PlayerSkinSelectionStands.cs
+++ Assets/Scripts/UI/PlayerSkinSelectionStands.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -7,17 +8,43 @@ public class PlayerSkinSelectionStands : MonoBehaviour
 {
     [SerializeField]
     private List<GameObject> stands;
+    [SerializeField]
+    private List<GameObject> modelsPositions;
+
+    private List<PlayerInput> playerInputs;
+
+    private bool allHaveDifferentSkins;
 
     private void Start()
     {
+        playerInputs = new List<PlayerInput>();
         foreach (var item in stands)
         {
             item.SetActive(false);
         }
-        PlayerInputManager.instance.onPlayerJoined += ActivateStand;    
+        PlayerInputManager.instance.onPlayerJoined += ActivateStand;
+        PlayerInputManager.instance.onPlayerJoined += TakeControlOfSpawnedPlayer;
     }
 
 
+    private void TakeControlOfSpawnedPlayer(PlayerInput playerInput)
+    {
+        playerInputs.Add(playerInput);
+
+        //foreach (var item in playerInput.GetComponents(typeof(Component)))
+        //{
+        //    Debug.Log(item.GetType());
+        //}
+
+        playerInput.GetComponent<Rigidbody>().isKinematic = true;
+        playerInput.GetComponent<PlayerMovement>().enabled = false;
+
+        playerInput.transform.position = modelsPositions[playerInputs.IndexOf(playerInput)].transform.position;
+        playerInput.transform.localScale = Vector3.one;
+        playerInput.GetComponent<PlayerShowcaseMode>().EnableShowcase();
+
+
+    }
 
     private void ActivateStand(PlayerInput playerInput)
     {
@@ -35,7 +62,7 @@ public class PlayerSkinSelectionStands : MonoBehaviour
     private void OnDisable()
     {
         PlayerInputManager.instance.onPlayerJoined -= ActivateStand;
-
+        PlayerInputManager.instance.onPlayerJoined -= TakeControlOfSpawnedPlayer;
     }
 
 
