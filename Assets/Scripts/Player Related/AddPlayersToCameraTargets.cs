e2e2de6413570f53c98eda06ccf5f14395a6e6f5
Letail <francisco.tadeu.torres@gmail.com>
2020-10-24 19:49:56 +0100

    Dir Sphere now doesn't cast shadows

--- Assets/Scripts/Player Related/AddPlayersToCameraTargets.cs
+++ Assets/Scripts/Player Related/AddPlayersToCameraTargets.cs
@@ -8,6 +8,9 @@ public class AddPlayersToCameraTargets : MonoBehaviour
     List<GameObject> targets;
     private GameObject visualsPrefab;
 
+    [Header("Testing Only")]
+    public bool SpawnPlayerOnJoined;
+
     void Awake()
     {
         targets = new List<GameObject>();
@@ -15,8 +18,18 @@ public class AddPlayersToCameraTargets : MonoBehaviour
 
     public void OnPlayerJoined(PlayerInput playerInput)
     {
-        visualsPrefab = playerInput.gameObject.GetComponent<SpawnCharacterVisualsPrefab>().GetPrefab();
-        //visualsPrefab = playerInput.gameObject.GetComponent<SpawnCharacterVisualsPrefab>().Spawn();
+        //This if is for testin only.
+        //This method should only call GetPrefab(), never Spawn.
+        if(SpawnPlayerOnJoined)
+        {
+            visualsPrefab = playerInput.gameObject.GetComponent<SpawnCharacterVisualsPrefab>().Spawn();
+        }
+        else
+        {
+            visualsPrefab = playerInput.gameObject.GetComponent<SpawnCharacterVisualsPrefab>().GetPrefab();
+        }
+
+        
         if (visualsPrefab != null)
         {
             targets.Add(visualsPrefab);
